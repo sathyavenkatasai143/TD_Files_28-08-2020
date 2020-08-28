bbc56a4f58f8a9e4962e112d338d124b15956a77
sathyavenkatasai143 <60285860+sathyavenkatasai143@users.noreply.github.com>
2020-08-28 17:46:55 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Front End Development'
+copyright = '2020, Sathya'
+author = 'Sathya'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
